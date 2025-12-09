894166eafc64e2b6f92ea1a6bb7527f427f9eae7
AH Muzahid <ahmuzahid40@gmail.com>
2025-12-09 23:33:47 +0600

    feat: add jwt token generation api and secure cookie configuration

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 
+  [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5173',
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // MongoDB Connection
 
@@ -27,6 +38,78 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Create database and collection
+    const database = client.db("LoanLinkDB");
+    const usersCollection = database.collection("users");
+    // Auth Related APIs
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: '1h'
+      });
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+        .send({ success: true });
+    });
+
+    // Logout API
+    app.post('/logout', (req, res) => {
+      const user = req.body;
+      console.log("logging out", user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true });
+    });
+
+    // Verify JWT Middleware
+    const verifyJWT = (req, res, next) => {
+      const token = req.cookies.token;
+      if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+      }
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'Unauthorized access' });
+        }
+        req.user = decoded;
+        next();
+      });
+    };
+
+
+    // Create a user 
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'User already exists' });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Get all users
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -40,10 +123,10 @@ run().catch(console.dir);
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('LoanLink Server is Running');
+  res.send('LoanLink Server is Running');
 });
 
 app.listen(port, () => {
-    console.log(`LoanLink is running on port: ${port}`);
+  console.log(`LoanLink is running on port: ${port}`);
 });
 
